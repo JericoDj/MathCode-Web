9caaf6ae75ff4f8e4acd3f93fec15f2519c98c49
jericodejesus <dejesusjerico528@gmail.com>
2025-10-20 15:55:17 +0800

    .env firebase

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -4,15 +4,16 @@ import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration from environment variables
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBRG20i_XdolLNdP6KTaGV0IauWS_IlIqg",
-  authDomain: "mathcode-1c100.firebaseapp.com",
-  projectId: "mathcode-1c100",
-  storageBucket: "mathcode-1c100.firebasestorage.app",
-  messagingSenderId: "315820356537",
-  appId: "1:315820356537:web:195cb37bd8981cf1967c68",
-  measurementId: "G-F0LFXW6B8G"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
